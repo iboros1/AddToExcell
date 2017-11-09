1e6c68188002d012a1d27dac260287f29cca3649
Istvan Boros <istvan.boros@3pillarglobal.com>
2017-11-09 18:13:38 +0200

    ads

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import urllib.request
 from bs4 import BeautifulSoup
-import openpyxl
 
-# url1 = input("add url:", )
-url = 'https://www.olx.ro/oferta/duplex-de-vanzare-faget-cluj-ID91DKR.html#d31a30b111'
+
+url = input("add url:", )
+#url = 'https://www.olx.ro/oferta/duplex-de-vanzare-faget-cluj-ID91DKR.html#d31a30b111'
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 soup.prettify()
@@ -12,12 +12,14 @@ image_location = soup.find(attrs={'id': ['photo-gallery-opener']})
 title_location = soup.find(attrs={'class':['offer-titlebox']})
 content = soup.find(attrs={'class':['descriptioncontent']})
 user_field = soup.find(attrs={'class':['offer-user__details']})
+price_label = soup.find(attrs={'class':['price-label']})
 
 image = image_location.contents[1].attrs['src']
 title = title_location.contents[1].contents[0].strip()
 publicat_de_pe = content.contents[8].text.strip()
 description = content.contents[6].contents[1].contents[0].strip()
 user = user_field.contents[1].contents[1].contents[0].strip()
+price = price_label.contents[1].contents[0]
 
 try:
     publicat_de = content.contents[6].contents[1].contents[8].strip()
@@ -25,13 +27,6 @@ except:
     publicat_de = "Not Specified"
 
 
-wb = openpyxl.load_workbook("file.xlsx")
-sheet = wb.get_sheet_by_name('Sheet1')
-
-if sheet.cell(row = 1, column=7).value == None:
-    print("Blank")
-else:
-    print("No blank")
 
 
 print(image)
